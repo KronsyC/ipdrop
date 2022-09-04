484eea9ed24d8a2907cfb8e412e689b1661622b8
caseyallen <caseyallen1024@gmail.com>
2022-09-04 19:49:56 +0000

    experimenting without gcp

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -109,18 +109,20 @@ async function createUpload(req: NextApiRequest, res: NextApiResponse) {
                 })
                 console.log("wrote to db");
 
-                const fileBuffer = Buffer.from(body.data, "base64");
-                console.log("bufferized");
+                // const fileBuffer = Buffer.from(body.data, "base64");
+                // console.log("bufferized");
 
-                const file = bucket.file(fileHash);
+                // const file = bucket.file(fileHash);
 
-                console.log("wrote to bucket");
-                file.on("error", () => {
-                    console.log("error");
-                    res.status(500).send("GCP ERROR")
+                // console.log("wrote to bucket");
+                // file.on("error", () => {
+                //     console.log("error");
+                //     res.status(500).send("GCP ERROR")
                     
-                })
-                file.save(fileBuffer);
+                // })
+                // file.save(fileBuffer, {
+                //     gzip: true
+                // });
                 uploadId = fileRef.id;
             }
             catch {
